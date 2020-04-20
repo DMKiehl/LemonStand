0fd26e526c1ef9418543a3f16fcf5a41e4c1ac0f
Dana Kiehl <dana.palm87@gmail.com>
2020-04-20 16:02:24 -0500

    Created a CustomerPreferences method in teh customer class

--- LemonadeStand/Customer.cs
+++ LemonadeStand/Customer.cs
@@ -11,15 +11,41 @@ namespace LemonadeStand
         //member variables
         private List<string> names;
         public string name;
+        Random myRandom = new Random();
+        public int maxCupsCustomerWillBuy;
+        public int tempThreshold;
+        public double priceThreshold;
 
         //constructor
         public Customer()
         {
             names = new List<string> { "Dana", "Brad", "Fred", "Paul", "Chloe", "Shannon", "Teresa", "Aaron", "Brian", "Amy", "Ethan", "Dave", "Laurie", "Ken", "Lisa", "Kelly", "Tom", "Tony", "Trevor" };
+            CustomerPreferences();
 
         }
 
         //member methods
+        public void CustomerPreferences()
+        {
+            //assign name
+            int index = myRandom.Next(names.Count);
+            name = (names[index]);
+
+            int min = 50;
+            int max = 100;
+
+            //assign temp
+            tempThreshold = myRandom.Next(min, max);
+
+            //assign price threshold
+            priceThreshold = myRandom.NextDouble();
+
+            
+               
+
+
+        }
+            
 
     }
 }
